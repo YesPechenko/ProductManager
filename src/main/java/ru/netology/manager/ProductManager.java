a116b46723499fc97c1dd535bf2082474d090af4
Figurova Olga <yespechenko@gmail.com>
2020-12-11 13:24:12 +0300

    branch manager3

--- src/main/java/ru/netology/manager/ProductManager.java
+++ src/main/java/ru/netology/manager/ProductManager.java
@@ -21,7 +21,8 @@ public class ProductManager {
            for (Product product: repository.findAll()) {
                if (matches(product, text)) {
                     Product[] tmp = new Product[result.length + 1];
-                    // используйте System.arraycopy, чтобы скопировать всё из result в tmp
+                    System.arraycopy(result,0, tmp, 0, result.length);
+
                     tmp[tmp.length - 1] = product;
                     result = tmp;
                 }
@@ -38,7 +39,9 @@ public class ProductManager {
             if (book.getAuthor().equalsIgnoreCase(search)) {
                 return true;
             }
+            return false;
         }
+
         if (product instanceof Smartphone) {
             Smartphone smartphone = (Smartphone) product;
             if (smartphone.getName().equalsIgnoreCase(search)) {
--- src/test/java/ru/netology/manager/ProductManagerTest.java
+++ src/test/java/ru/netology/manager/ProductManagerTest.java
@@ -13,10 +13,12 @@ class ProductManagerTest {
     ProductManager manager = new ProductManager(repository);
     Book first = new Book(1, "Name1", 10, "Author1");
     Book second = new Book(2, "Name2", 20, "Author2");
-    Book third = new Book(3, "Name3", 30, "Author3");
+    Book third1 = new Book(3, "Name3", 30, "Author3");
+    Book third2 = new Book(5, "Name4", 40, "Author3");
     Smartphone fourth = new Smartphone(4, "NameS1", 100, "Generator1");
     Smartphone fifth = new Smartphone(5, "NameS2", 200, "Generator2");
-    Smartphone sixth = new Smartphone(6, "NameS3", 300, "Generator3");
+    Smartphone sixth1 = new Smartphone(6, "NameS3", 300, "Generator3");
+    Smartphone sixth2 = new Smartphone(7, "NameS4", 400, "Generator3");
 
 
     @Test
@@ -29,8 +31,8 @@ class ProductManagerTest {
 
     @Test
     public void shouldSearchBySmarthoneGenerator() {
-        manager.add(sixth);
-        Product[] expected = new Product[]{sixth};
+        manager.add(sixth1);
+        Product[] expected = new Product[]{sixth1};
         Product[] actual = manager.searcyBy("Generator3");
         assertArrayEquals(expected, actual);
     }
@@ -54,12 +56,11 @@ class ProductManagerTest {
 
     @Test
     void shouldSearchBySomeBookAuthor () {
-        manager.add(first);
-        manager.add(second);
-        manager.add(third);
+        manager.add(third1);
+        manager.add(third2);
 
-        Product[] expected = new Product[]{second};
-        Product[] actual = manager.searcyBy("Author2");
+        Product[] expected = new Product[]{third1, third2};
+        Product[] actual = manager.searcyBy("Author3");
         assertArrayEquals(expected, actual);
     }
 
@@ -67,9 +68,9 @@ class ProductManagerTest {
     void shouldSearchBySomeBookName () {
         manager.add(first);
         manager.add(second);
-        manager.add(third);
+        manager.add(third1);
 
-        Product[] expected = new Product[]{third};
+        Product[] expected = new Product[]{third1};
         Product[] actual = manager.searcyBy("Name3");
         assertArrayEquals(expected, actual);
     }
@@ -83,11 +84,10 @@ class ProductManagerTest {
     }
     @Test
     void shouldSearchBySomeSmarthoneGenerator () {
-        manager.add(fourth);
-        manager.add(fifth);
-        manager.add(sixth);
+        manager.add(sixth1);
+        manager.add (sixth2);
 
-        Product[] expected = new Product[]{sixth};
+        Product[] expected = new Product[]{sixth1, sixth2};
         Product[] actual = manager.searcyBy("Generator3");
         assertArrayEquals(expected, actual);
     }
@@ -96,7 +96,7 @@ class ProductManagerTest {
     void shouldSearchBySomeSmarthoneName () {
         manager.add(fourth);
         manager.add(fifth);
-        manager.add(sixth);
+        manager.add(sixth1);
 
         Product[] expected = new Product[]{fourth};
         Product[] actual = manager.searcyBy("NameS1");
